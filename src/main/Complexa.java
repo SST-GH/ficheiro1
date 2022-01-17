9c6b9a71c7cc13b1bc4995ed58f5ba1bfa9882b3
jbarrinhos <jessjbarros@gmail.com>
2022-01-17 15:30:01 +0000

    complex

--- src/main/Complexa.java
+++ src/main/Complexa.java
@@ -3,33 +3,23 @@ package main;
 import java.util.Scanner;
 
 public class Complexa {
-	
-	public static void main(String [] arg) {
-		
-		Scanner dadosUt = new Scanner(System.in);
-		
-		int numUm = dadosUt.nextInt();
-		System.out.println("Insira primeiro numero: " + numUm);
-		
-		int numDois = dadosUt.nextInt();
-		System.out.println("Insira segundo numero: " + numDois);
+
+	public static void main(String[] arg) {
+
+		System.out.println("Digite dois numeros");
+
+		Scanner multi = new Scanner(System.in);
+
+		int numA = multi.nextInt();
+		int numB = multi.nextInt();
+
+		int a = (numA * numB) + (numA - numB);
+
+		System.out.println("Resultado " + a);
 	}
 }
 
+//Crie uma funï¿½ï¿½o chamada operaï¿½ï¿½oComplexa() que recebe dois nï¿½meros e retorna a soma da multiplicaï¿½ï¿½o desses mesmos dois nï¿½meros 
+//com a subtraï¿½ï¿½o desses dois nï¿½meros. Deverï¿½ utilizar as funï¿½ï¿½es jï¿½ definidas previamente. 
 
-
-
-
-
-
-
-
-
-
-//Crie uma função chamada operaçãoComplexa() que recebe dois números e retorna a soma da multiplicação desses mesmos dois números 
-//com a subtração desses dois números. Deverá utilizar as funções já definidas previamente. 
-
-//multiplicação(a,b)+ subtração(a,b)
-//multiplicaçãoa,b+ subtração(a,b)
- 
- 
+//multiplicaï¿½ï¿½o(a,b)+ subtraï¿½ï¿½o(a,b)
