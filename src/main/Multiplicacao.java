7dad33064d659fdedc431bd30b6639e246474725
jbarrinhos <jessjbarros@gmail.com>
2022-01-17 15:06:27 +0000

    multi_done

---
+++ src/main/Multiplicacao.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Multiplicacao {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 2;
+		// variação 1
+		int q = (x * y);
+		System.out.println("Resultado = " + q);
+	}
+}
