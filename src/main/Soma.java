4ab7b85f4d07c2177570df3a6a26729bda4ee4de
studentsara <97111715+studentsara@users.noreply.github.com>
2022-01-17 15:09:27 +0000

    sub

--- src/main/Soma.java
+++ src/main/Soma.java
@@ -2,7 +2,7 @@ package main;
 
 public class Soma {
 	
-	public static void {
+	public static void main (String[] args){
 		
 		int soma = 20 + 20;
 		System.out.println("Resultado: " + soma);
--- src/main/Subtracao.java
+++ src/main/Subtracao.java
@@ -1,5 +1,10 @@
 package main;
 
 public class Subtracao {
+public static void main (String[] args){
+		
+		int subtracao = 20 - 20;
+		System.out.println("Resultado: " + subtracao);
+	}
 
 }
