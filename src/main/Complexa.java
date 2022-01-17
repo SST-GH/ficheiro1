d9430842844fbd323b462e63d85223fd6d8d968b
studentsara <97111715+studentsara@users.noreply.github.com>
2022-01-17 15:16:21 +0000

    info questao

--- src/main/Complexa.java
+++ src/main/Complexa.java
@@ -3,3 +3,21 @@ package main;
 public class Complexa {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+//Crie uma função chamada operaçãoComplexa() que recebe dois números e retorna a soma da multiplicação desses mesmos dois números 
+//com a subtração desses dois números. Deverá utilizar as funções já definidas previamente. 
+
+//multiplicação(a,b)+ subtração(a,b)
+//multiplicaçãoa,b+ subtração(a,b)
+ 
+ 
